7d5df2a03f7f401a6129fc929fd2e8b0b1dd884f
liu20010110 <e45v92h56@gmail.com>
2022-05-18 18:57:19 +0800

    En first pod lib

---
+++ Example/Pods/Target Support Files/TextfieldPodLibEn/TextfieldPodLibEn-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_TextfieldPodLibEn : NSObject
+@end
+@implementation PodsDummy_TextfieldPodLibEn
+@end
---
+++ TextfieldPodLibEn/Classes/TextfieldFunc.swift
@@ -0,0 +1,20 @@
+//
+//  TextfieldFunc.swift
+//  Pods-TextfieldPodLibEn_Example
+//
+//  Created by 劉陶恩 on 2022/5/18.
+//
+
+import Foundation
+
+public class TestFunc {
+    
+    public static func sayHello() {
+        print("MyFirstPodLib say Hello！")
+    }
+    
+    public static func inputAndOutput(input: String?) {
+        print("MyFirstPodLib：You input \(String(describing: input)), and I output \(String(describing: input))")
+    }
+    
+}
